f86900dfaddadf987b41aa7b90044ea514081e31
Crashkins <bastien.jolidon@epfl.ch>
2023-03-18 19:19:05 +0100

    small graphical improvement

--- src/main/scala/terverak/view/PlayerView.scala
+++ src/main/scala/terverak/view/PlayerView.scala
@@ -16,13 +16,20 @@ import terverak.viewmodel.*
   */
 object PlayerView {
   
+  /**
+    * Draws a player.
+    * 
+    * @param player the player to draw
+    * @param playerViewModel the view model of the player
+    * @return the batch of the player
+    */
   def draw(player: Player, playerViewModel: PlayerViewModel): Batch[SceneNode] = {
     val x = playerViewModel.position.x
     val y = playerViewModel.position.y
 
     Batch(
       Graphic(x, y, PlayerViewModel.HeroSize.width, PlayerViewModel.HeroSize.height, 100, Material.Bitmap(playerViewModel.heroPicture)),
-      Shape.Box(Rectangle(x + 1, y + PlayerViewModel.HeroSize.height - 9, 8, 8), Fill.Color(RGBA.Teal)).withDepth(Depth(90)),
+      Shape.Box(Rectangle(x + 1, y + PlayerViewModel.HeroSize.height - 9, 16, 8), Fill.Color(RGBA.Teal)).withDepth(Depth(90)),
       Shape.Box(Rectangle(x + PlayerViewModel.HeroSize.width - 9, y + PlayerViewModel.HeroSize.height - 9, 8, 8), Fill.Color(RGBA.Teal)).withDepth(Depth(90)),
       Text(player.healthPoints.toString(), x + 1, y + PlayerViewModel.HeroSize.height - 9, 80, GameAssets.Fonts.fontNormal8Key, GameAssets.Fonts.fontNormal8Material.withTint(RGBA.Red)),
       Text(player.mana.toString(), x + PlayerViewModel.HeroSize.width - 9, y + PlayerViewModel.HeroSize.height - 9, 80, GameAssets.Fonts.fontNormal8Key, GameAssets.Fonts.fontNormal8Material.withTint(RGBA.Blue)),
