6e5eedeb72fc1594a9b3b01a464b7a45d6654dc6
KelvinKappeler <kelvin.kappeler@epfl.ch>
2023-04-19 15:05:15 +0200

    Update MenuSceneModel.scala

--- src/main/scala/terverak/scenes/menu/MenuSceneModel.scala
+++ src/main/scala/terverak/scenes/menu/MenuSceneModel.scala
@@ -10,6 +10,7 @@ import indigo.*
 import indigo.scenes.*
 import terverak.scenes.chooseDeck.*
 import terverak.scenes.deckCollection.*
+import terverak.scenes.play.PlayScene
 
 /**
   * The model of the menu scene.
@@ -18,7 +19,7 @@ final class MenuSceneModel {
 
   def updateModel(context: SceneContext[Unit]): GlobalEvent => Outcome[MenuSceneModel] = {
     case KeyboardEvent.KeyDown(Key.SPACE) =>
-      Outcome(this).addGlobalEvents(SceneEvent.JumpTo(ChooseDeckScene.name))
+      Outcome(this).addGlobalEvents(SceneEvent.JumpTo(PlayScene.name))
     case KeyboardEvent.KeyDown(Key.KEY_E) =>
       Outcome(this).addGlobalEvents(SceneEvent.JumpTo(DeckCollectionScene.name))
     case _ => Outcome(this)
