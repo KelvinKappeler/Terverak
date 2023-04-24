6e72844c4e56705c6b868ef3185b51471ef59efa
KelvinKappeler <kelvin.kappeler@epfl.ch>
2023-04-24 17:34:35 +0200

    Revert "Update CardEffect.scala"
    This reverts commit 73417eba2e628fba6f0687eed0a65747a4f87f1f.

--- src/main/scala/terverak/card/cardeffect/CardEffect.scala
+++ src/main/scala/terverak/card/cardeffect/CardEffect.scala
@@ -19,11 +19,8 @@ trait CardEffect {
     */
   def activateEffect(game: Game, target: Option[MinionWithId]): Game
 
-  //def targetType: CardEffectTarget = CardEffectTarget.None
+  def targetType: CardEffectTarget = CardEffectTarget.None
 
   override def toString: String = "Unknown effect"
 }
 
-trait CardEffectWithTarget extends CardEffect {
-  def targetType: CardEffectTarget = CardEffectTarget.Minion
-}
